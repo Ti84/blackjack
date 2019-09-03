94aef327460b755f37bb861ffc77c9194b47bdfc
Shane4666 <sc897@nau.edu>
2019-09-02 20:08:03 -0700

    deck data.

---
+++ src/data/deck.js
@@ -0,0 +1,313 @@
+export const deck = [
+{
+  id: 1,
+  value: 1,
+  name: 'A',
+  suite: 'hearts'
+},
+{
+  id: 2,
+  value: 1,
+  name: 'A',
+  suite: 'diamonds'
+},
+{
+  id: 3,
+  value: 1,
+  name: 'A',
+  suite: 'spades'
+},
+{
+  id: 4,
+  value: 1,
+  name: 'A',
+  suite: 'clubs'
+},
+{
+  id: 5,
+  value: 2,
+  name: '2',
+  suite: 'hearts'
+},
+{
+  id: 6,
+  value: 2,
+  name: '2',
+  suite: 'diamonds'
+},
+{
+  id: 7,
+  value: 2,
+  name: '2',
+  suite: 'spades'
+},
+{
+  id: 8,
+  value: 2,
+  name: '2',
+  suite: 'clubs'
+},
+{
+  id: 9,
+  value: 3,
+  name: '3',
+  suite: 'hearts'
+},
+{
+  id: 10,
+  value: 3,
+  name: '3',
+  suite: 'diamonds'
+},
+{
+  id: 11,
+  value: 3,
+  name: '3',
+  suite: 'spades'
+},
+{
+  id: 12,
+  value: 3,
+  name: '3',
+  suite: 'clubs'
+},
+{
+  id: 13,
+  value: 4,
+  name: '4',
+  suite: 'hearts'
+},
+{
+  id: 14,
+  value: 4,
+  name: '4',
+  suite: 'diamonds'
+},
+{
+  id: 15,
+  value: 4,
+  name: '4',
+  suite: 'spades'
+},
+{
+  id: 16,
+  value: 4,
+  name: '4',
+  suite: 'clubs'
+},
+{
+  id: 17,
+  value: 5,
+  name: '5',
+  suite: 'hearts'
+},
+{
+  id: 18,
+  value: 5,
+  name: '5',
+  suite: 'diamonds'
+},
+{
+  id: 19,
+  value: 5,
+  name: '5',
+  suite: 'spades'
+},
+{
+  id: 20,
+  value: 5,
+  name: '5',
+  suite: 'clubs'
+},
+{
+  id: 21,
+  value: 6,
+  name: '6',
+  suite: 'hearts'
+},
+{
+  id: 22,
+  value: 6,
+  name: '6',
+  suite: 'diamonds'
+},
+{
+  id: 23,
+  value: 6,
+  name: '6',
+  suite: 'spades'
+},
+{
+  id: 24,
+  value: 6,
+  name: '6',
+  suite: 'clubs'
+},
+{
+  id: 25,
+  value: 7,
+  name: '7',
+  suite: 'hearts'
+},
+{
+  id: 26,
+  value: 7,
+  name: '7',
+  suite: 'diamonds'
+},
+{
+  id: 27,
+  value: 7,
+  name: '7',
+  suite: 'spades'
+},
+{
+  id: 28,
+  value: 7,
+  name: '7',
+  suite: 'clubs'
+},
+{
+  id: 29,
+  value: 8,
+  name: '8',
+  suite: 'hearts'
+},
+{
+  id: 30,
+  value: 8,
+  name: '8',
+  suite: 'diamonds'
+},
+{
+  id: 31,
+  value: 8,
+  name: '8',
+  suite: 'spades'
+},
+{
+  id: 32,
+  value: 8,
+  name: '8',
+  suite: 'clubs'
+},
+{
+  id: 33,
+  value: 9,
+  name: '9',
+  suite: 'hearts'
+},
+{
+  id: 34,
+  value: 9,
+  name: '9',
+  suite: 'diamonds'
+},
+{
+  id: 35,
+  value: 9,
+  name: '9',
+  suite: 'spades'
+},
+{
+  id: 36,
+  value: 9,
+  name: '9',
+  suite: 'clubs'
+},
+{
+  id: 37,
+  value: 10,
+  name: 'J',
+  suite: 'hearts'
+},
+{
+  id: 38,
+  value: 10,
+  name: 'J',
+  suite: 'diamonds'
+},
+{
+  id: 39,
+  value: 10,
+  name: 'J',
+  suite: 'spades'
+},
+{
+  id: 40,
+  value: 10,
+  name: 'J',
+  suite: 'clubs'
+},
+{
+  id: 41,
+  value: 10,
+  name: 'Q',
+  suite: 'hearts'
+},
+{
+  id: 42,
+  value: 10,
+  name: 'Q',
+  suite: 'diamonds'
+},
+{
+  id: 43,
+  value: 10,
+  name: 'Q',
+  suite: 'spades'
+},
+{
+  id: 44,
+  value: 10,
+  name: 'Q',
+  suite: 'clubs'
+},
+{
+  id: 45,
+  value: 10,
+  name: 'K',
+  suite: 'hearts'
+},
+{
+  id: 46,
+  value: 10,
+  name: 'K',
+  suite: 'diamonds'
+},
+{
+  id: 47,
+  value: 10,
+  name: 'K',
+  suite: 'spades'
+},
+{
+  id: 48,
+  value: 10,
+  name: 'K',
+  suite: 'clubs'
+},
+{
+  id: 49,
+  value: 10,
+  name: '10',
+  suite: 'hearts'
+},
+{
+  id: 50,
+  value: 10,
+  name: '10',
+  suite: 'diamonds'
+},
+{
+  id: 51,
+  value: 10,
+  name: '10',
+  suite: 'spades'
+},
+{
+  id: 52,
+  value: 10,
+  name: '10',
+  suite: 'clubs'
+}];
